097259a2c38e834c80c315369270a8fb0e9d1b20
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-09 00:57:19 +0000

    Practica_1

---
+++ _1_La_función_print()_ y_palabras_claves.py
@@ -0,0 +1,22 @@
+#Con esta linea se imprime un saludo en la pantalla
+print("¡Hola, mundo!")
+
+#Mientras que esta linea imprime el nombre del usuario
+print("Jesus Ismael Flores Pardo")
+
+#En estos casos manda anda un error de identidada inesperada
+# print(Jesus Ismael Flores Pardo)
+# print"Jesus Ismael Flores Pardo"
+# print('Jesus Ismael Flores Pardo')
+# print(Jesus Ismael Flores Pardo) print("¡Hola, mundo!")
+
+#Python ofrece otro mecanismo para el paso de argumentos, que puede ser útil cuando 
+# deseas convencer a la función print() para que cambie un poco su comportamiento. Uno de 
+# ellos 'end': cualquier argumento de palabra clave debe colocarse después del último 
+# argumento posicional; y sep que funciona como separador
+
+
+# Esta linea lo que hace es imprimer un los textos que aparecen con doble comilla, con una 
+# separación indicadada por sep que sera *** y al final del argumento genera ...
+print("Programming","Essentials","in",sep="***",end="...")
+print("Monty","Phyton")
