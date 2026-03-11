31b802efa1abf6075d290414e290351ba6abc946
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 07:08:20 +0000

    Practica 3

--- _3_Dando_forma_a_la_salida.py
+++ _3_Dando_forma_a_la_salida.py
@@ -1,21 +1,19 @@
-#El conjuto de estos 8 prints, imprimen una flecha hecha con los asteristcos
+
 print("    *")
 print("   * *")
 print("  *   *")
 print(" *     *")
-print("***   ***")
+print("***   ***") #Cada uno de estos print genera una linea la cual las 8 juntas hacen una flecha
 print("  *   *")
 print("  *   *")
 print("  *****")
 
-#Si queremos reducir la cantidad de prints podemos usar la variable \n la cual genera 
-#un salto de linea despues segun donde se hubiera puesto como el siguiente ejemplo
-print("\n",end="----------menor catidad de print----------\n")
-print("\n     *\n", "   * *\n", "  *   *\n", " *     *\n", "***   ***\n", "  *   *",)
-print("   *   *\n", "  *****\n")
 
-#En las siguientes lienas lo que imprimira en pantalla sera una flecha pero con el 
-#doble de grande que la original
+print("\n",end="----------menor catidad de print----------\n") #Genera un salto de linea yrl valor de salida sera una divicion para cada ejemplo
+print("\n     *\n", "   * *\n", "  *   *\n", " *     *\n", "***   ***\n", "  *   *",)
+print("   *   *\n", "  *****\n") #crean la misma flecha pero con menor cantidad de prints y utilizan saltos de linea para que se generen en sus respectivas lineas
+
+
 
 print("\n",end="----------Doble de grande----------\n")
 print("        *")
@@ -26,7 +24,7 @@ print("    *       *")
 print("   *         *")
 print("  *           *")
 print(" *             *")
-print("******     ******")
+print("******     ******") #Este ejemplo es parecido al primero pero se aumenta su tamaño al doble
 print("     *     *")
 print("     *     *")
 print("     *     *")
@@ -35,18 +33,16 @@ print("     *     *")
 print("     *     *")
 print("     *******")
 
-#En las siguientes lienas lo que imprimira en pantalla seran dos flechas esto se logra con 
-#la varible "string" *n (donde n son las veces que que queremos que se multiplique), la cual 
-#en este caso usaremos 3l valor de 2 para duplicar la cadena.
+
 print("\n",end="----------String(Doble)Grande----------\n")
-n=2
+n=2  #declaramos la variable para las veces que queroms multiplicar la cadena
 print("        *        "*n)
 print("       * *       "*n)
 print("      *   *      "*n)
 print("     *     *     "*n)
 print("    *       *    "*n)
 print("   *         *   "*n)
-print("  *           *  "*n)
+print("  *           *  "*n) #Multiplica la cadena segun el valor de n
 print(" *             * "*n)
 print("******     ******"*n)
 print("     *     *     "*n)
@@ -57,15 +53,13 @@ print("     *     *     "*n)
 print("     *     *     "*n)
 print("     *******     "*n)
 
-#Como ejemplo extra haremos lo mismo para la flecha pequeña pero de mas multiplicaciones
-#podemos usar diferentes variables para declara las veces que queremos multiplicar no solo 
-#se limita con n
+#Como ejemplo extra
 print("\n",end="----------String *m pequeño----------\n")
-m=3
+m=3 #declaramos una segunda variable para las veces que queroms multiplicar la cadena
 print("    *    "*m)
 print("   * *   "*m)
 print("  *   *  "*m)
-print(" *     * "*m)
+print(" *     * "*m) #Multiplica la cadena segun el valor de m
 print("***   ***"*m)
 print("  *   *  "*m)
 print("  *   *  "*m)
