700a9297c9eb0af3d84d3ff45596f84176719ba7
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 05:09:52 +0000

    Practica_2_

---
+++ _1_La_función_print()_.py
@@ -0,0 +1,11 @@
+#Con esta linea se imprime un saludo en la pantalla
+print("¡Hola, mundo!")
+
+#Mientras que esta linea imprime el nombre del usuario
+print("Jesus Ismael Flores Pardo")
+
+#En estos casos manda anda un error de identidada inesperada
+# print(Jesus Ismael Flores Pardo)
+# print"Jesus Ismael Flores Pardo"
+# print('Jesus Ismael Flores Pardo')
+# print(Jesus Ismael Flores Pardo) print("¡Hola, mundo!")
--- _2_La funcion_print()_y_sus_argumentos.py
+++ _2_La funcion_print()_y_sus_argumentos.py
@@ -1,15 +1,3 @@
-#Con esta linea se imprime un saludo en la pantalla
-print("¡Hola, mundo!")
-
-#Mientras que esta linea imprime el nombre del usuario
-print("Jesus Ismael Flores Pardo")
-
-#En estos casos manda anda un error de identidada inesperada
-# print(Jesus Ismael Flores Pardo)
-# print"Jesus Ismael Flores Pardo"
-# print('Jesus Ismael Flores Pardo')
-# print(Jesus Ismael Flores Pardo) print("¡Hola, mundo!")
-
 #Python ofrece otro mecanismo para el paso de argumentos, que puede ser útil cuando 
 # deseas convencer a la función print() para que cambie un poco su comportamiento. Uno de 
 # ellos 'end': cualquier argumento de palabra clave debe colocarse después del último 
