41add791703e6ad297625737b00939386d437004
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 08:30:34 +0000

    Practica 2

--- _2_La funcion_print()_y_sus_argumentos.py
+++ _2_La funcion_print()_y_sus_argumentos.py
@@ -1,3 +1,4 @@
-print("Programming","Essentials","in",sep="***",end="...")#sep genera una separación indicadada que sera "***" y end genera una salida al final del argumento el cual sera "..."
+print("Programming","Essentials","in",sep="***",end="...")
+#sep genera una separación indicadada que sera "***" y end genera una salida al final del argumento el cual sera "..."
 
 print("Monty","Phyton")#Imprime las varibles que estan encerrados en las doble comillas
