9c7f7915f869e6654e542ced149c6924e4b99f1a
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 17:56:09 +0000

    Practica 10

---
+++ _10_Operadores_y_expreciones.py
@@ -0,0 +1,15 @@
+x = float(input("Ingresa el valor para x: "))#Se declara una variable x que tendrá el valor que el usuario quiera.
+y = 1/(x + 1/(x + 1/(x + 1/x)))#Se calcula el valor de "y" con la siguiente formula.
+print("y =", y)#Se imprime el valor de "y"
+
+x = float(input("Ingresa el valor para x: "))#Se declara una nueva variable x que tendrá el valor que el usuario quiera.
+y = 1/(x + 1/(x + 1/(x + 1/x)))#Se calcula el nuevo valor de "y".
+print("y =", y)#Se imprime el nuevo valor de "y"
+
+x = float(input("Ingresa el valor para x: "))#Se declara una nueva variable x que tendrá el valor que el usuario quiera.
+y = 1/(x + 1/(x + 1/(x + 1/x)))#Se calcula el nuevo valor de "y".
+print("y =", y)#Se imprime el nuevo valor de "y"
+
+x = float(input("Ingresa el valor para x: "))#Se declara una nueva variable x que tendrá el valor que el usuario quiera.
+y = 1/(x + 1/(x + 1/(x + 1/x)))
+print("y =", y)#Se imprime el nuevo valor de "y"
