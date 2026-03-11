0fa7085a6bc6f7840917558935240580bbe0da63
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 16:24:36 +0000

    Practica 1

--- _1_La_función_print().py
+++ _1_La_función_print().py
@@ -11,12 +11,15 @@ print("Jesus Ismael Flores Pardo")#Imprime el nombre del usuario
 # print(Jesus Ismael Flores Pardo) print("¡Hola, mundo!")
 
 
-print("\n",end="----------Otra forma----------\n")#Imprime un salto de linea y end genera una salida al final del argumento el cual sera un titulo con otro salto de linea
+print("\n",end="----------Otra forma----------\n")
+#Imprime un salto de linea y end genera una salida al final del argumento el cual sera un titulo con otro salto de linea
+
 print("¡Hola, mundo!","mi nombre es:", " Jesus Ismael Flores Pardo")#Imprime el saludo con el nombre
 
 
 print("\n",end="----------palabra clave end----------\n")
-print("¡Hola, mundo!","mi nombre es: ", end="Jesus Ismael Flores Pardo\n")#Imprime un saludo y end genera una salida al final del argumento el cual sera el nombre del ususario
+print("¡Hola, mundo!","mi nombre es: ", end="Jesus Ismael Flores Pardo\n")
+#Imprime un saludo y end genera una salida al final del argumento el cual sera el nombre del ususario
 
 
 #Existen diversas formas pero estos son algunos ejemplos
