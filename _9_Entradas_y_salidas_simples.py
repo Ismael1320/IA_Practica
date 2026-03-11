54c593a7ab3f9dad2ad1e4372dabddd0d9b374bf
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 17:41:07 +0000

    Practica 9

---
+++ _9_Entradas_y_salidas_simples.py
@@ -0,0 +1,14 @@
+a = float(input("Dame un valor para a: ")) #se crea una variable "a", la cual tendrá el valor que el usuario quiera
+
+b = float(input("Dame un valor para b: "))#se crea una variable "b", la cual tendrá el valor que el usuario quiera
+
+#str() nos ayuda a asar el resultado entero a la función print() como una sola cadena, sin utilizar las comas.
+print("Si se suman su resultado sera: "+ str(a + b))#Imprime el resultado de la suma de los dos números introducidos
+
+print("Si se restan su resultado sera: "+ str(a - b))#Imprime el resultado de la resta de los dos números introducidos 
+
+print("Si se suman su resultado sera: "+ str(a * b))#Imprime el resultado de la multiplicación de los dos números introducidos 
+
+print("Si se suman su resultado sera: "+ str(a / b))#Imprime el resultado de la división de los dos números introducidos
+
+print("\n¡Eso es todo, amigos!")
