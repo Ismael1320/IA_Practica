ba9589e972049de4e009ef1b191e121d3e6b9c67
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 07:17:51 +0000

    Practica 2

--- _2_La funcion_print()_y_sus_argumentos.py
+++ _2_La funcion_print()_y_sus_argumentos.py
@@ -1,10 +1,3 @@
-#Python ofrece otro mecanismo para el paso de argumentos, que puede ser útil cuando 
-# deseas convencer a la función print() para que cambie un poco su comportamiento. Uno de 
-# ellos 'end': cualquier argumento de palabra clave debe colocarse después del último 
-# argumento posicional; y sep que funciona como separador
+print("Programming","Essentials","in",sep="***",end="...")#sep genera una separación indicadada que sera "***" y end genera una salida al final del argumento el cual sera "..."
 
-
-# Esta linea lo que hace es imprimer un los textos que aparecen con doble comilla, con una 
-# separación indicadada por sep que sera *** y al final del argumento genera ...
-print("Programming","Essentials","in",sep="***",end="...")
-print("Monty","Phyton")
+print("Monty","Phyton")#Imprime las varibles que estan encerrados en las doble comillas
