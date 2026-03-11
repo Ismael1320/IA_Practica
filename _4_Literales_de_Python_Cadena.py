f2c8da229b7f44018f61bdf238b94d17d89a3bb8
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 07:00:18 +0000

    Practica 4

---
+++ _4_Literales_de_Python_Cadena.py
@@ -0,0 +1,11 @@
+
+print("\"Estoy\"\n","\"\"aprendiendo\"\"\n","\"\"\"Python\"\"\"\n")
+#Imprime el mensaje:
+#"Estoy"
+#""Aprendiendo""
+#"""Python"""
+
+#"\n" (ejecuta un salto de linea donde este posicionado)
+ 
+#" \" " (Generer comillas en nuestra cadena, y evita que el programa interprete esas 
+# comillas como una nueva cadena)
