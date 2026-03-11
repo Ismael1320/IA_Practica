5ae03709bb8897c877fcd3334eeb8930ac647726
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 19:41:54 +0000

    Practica 12

---
+++ _12_Pregunta_y_respuesta.py
@@ -0,0 +1,7 @@
+n = int(input("Dame un valor: "))#El ususario le asigna un valor a n
+
+print("¿",n," es mayor que 100?")#Imprime la pregunta para comparar si n es mayor que 100
+
+n = n >= 100 #Ahora el valor de n sera la respuesta de dicha comparación, n si es mayor es true y si n es menor es false
+
+print(n)#Imprime la respuesta de la comparación
