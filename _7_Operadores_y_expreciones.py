ccc0cc7a0fd277def3405d3889da58b33d6aabd7
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 16:46:45 +0000

    Practica 7

---
+++ _7_Operadores_y_expreciones.py
@@ -0,0 +1,14 @@
+x =  0 #declaramos un valor para x
+x = float(x) #declaramos que el valor de x va hacer flotante(decimal)
+y = 3*(x**3) - 2*(x**2) + 3*(x) - 1 #declramos un valor para "y" con la siguiente opreción la cual necesita el valor de la variable x  
+print("y =", y) #Imprime el valor de "y" en decimales ya que x se habia declarado como flotante
+
+x = 1 #declaramos una nueva variable x con otro valor diferente
+x = float(x)
+y = 3*(x**3) - 2*(x**2) + 3*(x) - 1 #declramos una nueva variable "y" la cual tendra un resultado diferente , ya que x es una variable diferente 
+print("y =", y) #Imprime el nuevo valor de y
+
+x = -1 #declaramos una nueva variable x con otro valor diferente
+x = float(x)
+y = 3*(x**3) - 2*(x**2) + 3*(x) - 1  #declramos una nueva variable "y" la cual tendra un resultado diferente , ya que x es una variable diferente 
+print("y =", y) #Imprime el nuevo valor de y
