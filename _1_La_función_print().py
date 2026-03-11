4d1ede3420a0c64247cc0911429e7133f8be1334
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-11 07:24:00 +0000

    Practica 1

--- _1_La_función_print().py
+++ _1_La_función_print().py
@@ -1,8 +1,8 @@
-#Con esta linea se imprime un saludo en la pantalla
-print("¡Hola, mundo!")
+#print se utiliza para imprimr lo que esta dentro de las doble comillas
+print("¡Hola, mundo!")#Con esta linea se imprime un saludo en la pantalla
 
-#Mientras que esta linea imprime el nombre del usuario
-print("Jesus Ismael Flores Pardo")
+
+print("Jesus Ismael Flores Pardo")#Imprime el nombre del usuario
 
 #En estos casos manda anda un error de identidada inesperada
 # print(Jesus Ismael Flores Pardo)
@@ -10,14 +10,13 @@ print("Jesus Ismael Flores Pardo")
 # print('Jesus Ismael Flores Pardo')
 # print(Jesus Ismael Flores Pardo) print("¡Hola, mundo!")
 
-#Si queremos forma que imprima el saludo con el nombre podemos hacer lo siguiente
-print("\n",end="----------Otra forma----------\n")
-print("¡Hola, mundo!","mi nombre es:", " Jesus Ismael Flores Pardo")
 
-#O tambien lo podemos hacer declarando argumentos de palabra clave como lo es el end, 
-#el cual nos permite colocaenviar una salida al final de nuestros argumentos posicionales.
+print("\n",end="----------Otra forma----------\n")#Imprime un salto de linea y end genera una salida al final del argumento el cual sera un titulo con otro salto de linea
+print("¡Hola, mundo!","mi nombre es:", " Jesus Ismael Flores Pardo")#Imprime el saludo con el nombre
+
 
 print("\n",end="----------palabra clave end----------\n")
-print("¡Hola, mundo!","mi nombre es: ", end="Jesus Ismael Flores Pardo\n")
+print("¡Hola, mundo!","mi nombre es: ", end="Jesus Ismael Flores Pardo\n")#Imprime un saludo y end genera una salida al final del argumento el cual sera el nombre del ususario
+
 
 #Existen diversas formas pero estos son algunos ejemplos
