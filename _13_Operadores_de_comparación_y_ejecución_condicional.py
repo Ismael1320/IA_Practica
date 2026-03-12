1ffe66358c1c22d0f205b0ba23626d027435240a
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-12 06:22:26 +0000

    Practica 13

---
+++ _13_Operadores_de_comparación_y_ejecución_condicional.py
@@ -0,0 +1,11 @@
+flor = input("Introduce el nombre de la flor: ")#El usuario introduce el nombre de una flor
+
+if flor == "ESPATIFILIO": #Se declara una variable "if" la cual ejecutara una acción siempre y cuando se cumpla la condición.
+    print("Si, ¡El ESPATIFILIO es la mejor planta de todos los tiempos!") #Si flor es igual a "Espatifilo" se imprimira el siguiente mensaje
+
+elif flor == "espatifilo": #Se declara una variable "elif" la cual ejecutara una acción simpre y cuando if hubiera sido falso y se cupla la codición que se especifica
+    print("No, ¡quiero un gran ESPATIFILIO!")#Si flor es igual a "espatifilo" se imprimira el siguiente mensaje
+
+else: #Se declara una variable "else" la cual ejecuta una acció simpre y cuando if y elif hubieran sido falsas
+    print("¡ESPATIFILIO!, ¡No", flor,"!")#Si flor es igual a otra respuesta diferente se imprimira este mensaje
+	
