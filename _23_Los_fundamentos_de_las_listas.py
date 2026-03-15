66a8408df2e0e19988e66e338e9c73f0f24e9b6a
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-15 18:10:43 +0000

    Practica 23

---
+++ _23_Los_fundamentos_de_las_listas.py
@@ -0,0 +1,10 @@
+hat_list = [1, 2, 3, 4, 5]  #Se crea una lista con 5 numeros iniciales.
+#          [0, 1, 2, 3, 4] ---->Posicion de los numeros en la lista
+
+hat_list[2] = int(input("Ingrese un nuevo valor al número central: ")) #Se remplaza el valor central de la lista (posición 2) con el número que ingreso el usuario.
+
+del hat_list[4] #Se elimina el utimo número de la lista (posición 4).
+
+print("La longitud de la lista es de: ", len(hat_list)) #Se muesta la cantidad de elementos que tien ahora la lista.
+
+print(hat_list) #Se imprime la lista final después de las modificaciones.
