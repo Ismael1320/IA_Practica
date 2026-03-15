baf3c73e1e260f841d2ce3557111d00753ff2fb3
Ismael1320 <jesus.floresp.22@gmail.com>
2026-03-15 06:59:55 +0000

    Practica 17

---
+++ _17_Fundamentos_del_bucle_for–contando_lentamente_(mississippily).py
@@ -0,0 +1,8 @@
+import time #Importa la librería time para p´poder time para poder usar funciones relacionadas con el tiempo, como pausas.
+
+for i in range(5): #Crea un ciclo que se repetira 5 veces, iniciando de 0 y termina hasta el 4
+    print(i+1,"Mississipi") #Imprime el número del contador (sumandole uno al cotador al inicio de cada repetición) más la palabra Mississipi
+    time.sleep(1) #Hace una pausa de 1 segundo antes de continuar con la siguiente repetición
+
+
+print("Lista o no, aquí vengo!")#Muestra el mensaje final cuando termina el conteo
